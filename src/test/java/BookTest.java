63ac8aabda625784e026a2cb8d289928c41a2f77
Stephen <stephen_h369@live.co.uk>
2020-08-25 16:50:50 +0100

    book class and tests written

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
+
+    private Book book1;
+
+    @Before
+    public void before() {
+        book1 = new Book("a book", "an author", "some genre");
+    }
+
+    @Test
+    public void hasTitle() {
+        assertEquals("a book", book1.getTitle());
+    }
+
+    @Test
+    public void hasAuthor() {
+        assertEquals("an author", book1.getAuthor());
+    }
+
+    @Test
+    public void hasGenre() {
+        assertEquals("some genre", book1.getGenre());
+    }
 }
