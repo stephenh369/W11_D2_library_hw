70cba56e4bcfeba368cf9c4d95d7800a00fc2cb7
Stephen <stephen_h369@live.co.uk>
2020-08-25 17:24:17 +0100

    added borrower class

---
+++ src/main/java/Borrower.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Borrower {
+    private ArrayList<Book> borrowedBooks;
+
+    public Borrower() {
+        this.borrowedBooks = new ArrayList<Book>();
+    }
+}
