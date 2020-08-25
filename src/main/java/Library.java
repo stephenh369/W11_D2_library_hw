dbf105e26a40aae44045be6fd37888a89addfb3f
Stephen <stephen_h369@live.co.uk>
2020-08-25 17:16:22 +0100

    library class setup, can add books and check total books in library

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+
 public class Library {
+    private int capacity;
+    private ArrayList<Book> books;
+
+    public Library(int capacity) {
+        this.capacity = capacity;
+        this.books = new ArrayList<Book>();
+    }
+
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+
+    public int totalBooks() {
+        return books.size();
+    }
+
+    public void addBook(Book book) {
+        this.books.add(book);
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    private Library library;
+    private Book book1;
+    private Book book2;
+    private Book book3;
+
+    @Before
+    public void before() {
+        library = new Library(10);
+        book1 = new Book("The Shining", "Stephen King", "Horror");
+        book2 = new Book("Calum's Road", "Roger Hutchinson", "Biography");
+        book3 = new Book("Do Android's Dream of Electric Sheep?", "Philip K. Dick", "Sci Fi");
+    }
+
+    @Test
+    public void canAddBooks() {
+        library.addBook(book1);
+        assertEquals(1, library.totalBooks());
+    }
 }
